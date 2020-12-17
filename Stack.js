1f6fffc5e8d08f19d846677c3f347e5c07ab87d1
Jason Howe <trisynth@gmail.com>
2020-12-17 09:06:52 -0500

    Data structures references.

---
+++ Stack.js
@@ -0,0 +1,12 @@
+const LinkedList = require('./LinkedList');
+
+class Stack {
+  constructor() {
+    this.stack = new LinkedList();
+  }
+  peek() {
+    return this.stack.head.data;
+  }
+}
+
+module.exports = Stack;
